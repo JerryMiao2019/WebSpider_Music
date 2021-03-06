1ad017bad8f4c2875722c50ea260f1aef47d9e3b
JerryMiao2019 <JerryMiao2019@outlook.com>
2021-03-06 22:41:11 +0800

    None

--- music/CloudMusic.py
+++ music/CloudMusic.py
@@ -1,17 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib import parse
 
 def seach_Music(name):
-    search = 'https://music.163.com/#/search/m/?s=%s&type=1'% name
+    data = {'wd': name}
+    post_data = parse.urlencode(data)
+    #print(post_data[3:])
+    search = 'https://music.163.com/#/search/m/?s=%E6%B5%B7%E9%98%94%E5%A4%A9%E7%A9%BA&type=1'#% post_data[3:]
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'
+        'referer': 'https://music.163.com/',
+        'Host': 'music.163.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     }
     response = requests.get(url=search, headers=headers)
     print(response)
-    soup = BeautifulSoup(response.text, 'lxml')
-    print(soup)
-    h_flag = soup.find_all('div', 'item f-cb')
-    h_flag_even = soup.find_all('div', 'item f-cb h-flag')
-    print('\n\n\n\n\n\n\n\n',h_flag, '\n', h_flag_even)
+    soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
+    #print(soup)
+    div = soup.find_all('div', class_='ztag j-flag')
+    print(div, '\n')
 
-seach_Music('this is it')
+seach_Music('真的爱你')
